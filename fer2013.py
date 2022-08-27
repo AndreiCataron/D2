dc4be4d1497b44f8cb73d2b5e19c8fe26686381d
Lenovo <andrei.cataron@gmail.com>
2022-08-27 22:51:02 +0300

    m-am incurcat la fer2013.py

--- fer2013.py
+++ fer2013.py
@@ -15,26 +15,24 @@ def prepare_data(img_folder):
     emotion_mapping = {'angry': 0, 'disgust': 1, 'fear': 2, 'happy': 3, 'sad': 4, 'surprise': 5, 'neutral':6}
 
     for dir1 in os.listdir(img_folder):
-        for file in os.listdir(os.path.join(img_folder, dir1)):
-            image_path = os.path.join(img_folder, dir1, file)
-            image = Image.open(image_path)
-            image = np.asarray(image)
-            image = np.reshape(image, (48, 48))
-            #print(image_path)
-            #print(image)
-            img_data_array.append(image)
-            class_name.append(emotion_mapping.get(dir1))
+      for file in os.listdir(os.path.join(img_folder, dir1)):
+        if not file.startswith('.'):
+          image_path = os.path.join(img_folder, dir1, file)
+          image = Image.open(image_path)
+          image = np.asarray(image)
+          img_data_array.append(image)
+          class_name.append(emotion_mapping.get(dir1))
 
     #print(img_data_array[0])
 
     return img_data_array, class_name
 
-def my_get_dataloaders(path='FER2013dataset', bs = 64, augment = True):
+def my_get_dataloaders(path='/content/drive/MyDrive/FER2013dataset', bs = 64, augment = True):
     xtrain, ytrain = prepare_data(os.path.join(path, 'train'))
     #######################
     indexes = list(range(len(xtrain)))
     np.random.shuffle(indexes)
-    indexes = indexes[:64*440]
+    indexes = indexes[:64*447]
     xtrain = [xtrain[i] for i in indexes]
     ytrain = [ytrain[i] for i in indexes]
     #print(ytrain[:100])
@@ -75,7 +73,7 @@ def my_get_dataloaders(path='FER2013dataset', bs = 64, augment = True):
 
     test_transform = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize(mean=(0, ), std=(255,))
+        transforms.Normalize(mean=(0,), std=(255,))
     ])
 
     train = Dataset(xtrain, ytrain, train_transform)
@@ -88,4 +86,3 @@ def my_get_dataloaders(path='FER2013dataset', bs = 64, augment = True):
     testloader = DataLoader(test, batch_size=bs, shuffle=True, num_workers=0)
 
     return trainloader, valloader, testloader
-
